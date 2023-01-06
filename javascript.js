fe625edce5ce7b9e98b7037b67ce3d4fe70aa6dc
EzequisevlA <89744037+EzequisevlA@users.noreply.github.com>
2023-01-06 04:09:31 -0300

    first commit

---
+++ javascript.js
@@ -0,0 +1,32 @@
+const typedtextspan = document.querySelector(".typedtext")
+const textarray=["Frontend", "Developer"]
+const tipyngDelay = 200
+const erasingDelay = 100
+const newtextDelay = 2000
+let  textarrayindex = 0
+let charindex = 0
+
+
+function type(){
+    if(charindex< textarray[textarrayindex].length){
+        typedtextspan.textContent += textarray[textarrayindex].charAt(charindex)
+        charindex++
+        setTimeout(type, tipyngDelay)
+    }else{
+        setTimeout(erase, newtextDelay)
+    }
+}
+function erase(){
+    if(charindex >0){
+        typedtextspan.textContent = textarray[textarrayindex].substring(0, charindex-1)
+        charindex--
+        setTimeout(erase, erasingDelay)
+    }else{
+        textarrayindex++
+        if(textarrayindex>=textarray.length) textarrayindex=0
+        setTimeout(type, tipyngDelay + 1100)
+    }
+}
+document.addEventListener("DOMContentLoaded", function(){
+ setTimeout(type, newtextDelay +250)
+})
